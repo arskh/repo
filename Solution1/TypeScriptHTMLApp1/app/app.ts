d4e6f7c4f7adeb749710eeeae34ee20a0abdce55
Dombovári András <lanzarot@gmail.com>
2017-10-20 23:35:05 +0200

    nagyon alapok

---
+++ Solution1/TypeScriptHTMLApp1/app/app.ts
@@ -0,0 +1,4 @@
+﻿module app {
+    angular.module("app", [])
+        .controller("Controller1", Controller1);
+}
---
+++ Solution1/TypeScriptHTMLApp1/app/file1.ts
@@ -0,0 +1,19 @@
+﻿module app {
+    export class Controller1 {
+        hello: string;
+        menuVisible: boolean;
+
+        static $inject = [];
+
+        constructor(
+
+        ) {
+            this.hello = "Hello szia szevasz működök bazze";
+            this.menuVisible = false;
+        }
+
+        menuClicked() {
+            this.menuVisible = !this.menuVisible;
+        }
+    }
+}
